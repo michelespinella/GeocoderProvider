8f13a70d5fce8e0a32e6ea6102f2493590e2259c
michele <michele@ortro>
2020-05-15 08:38:05 +0200

    ms:removed some imports unused

--- src/main/java/it/corvallis/geocoder/utils/GeocoderProviderMapQ.java
+++ src/main/java/it/corvallis/geocoder/utils/GeocoderProviderMapQ.java
@@ -1,9 +1,7 @@
 package it.corvallis.geocoder.utils;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Iterator;
