a5d1e41d5c87c45985779afe980a7c845aa0a36d
SpinellaM <SpinellaM@cornb7228.informsrl.it>
2020-05-18 17:40:39 +0200

    ms: fixed code format

--- src/main/java/it/corvallis/geocoder/utils/App.java
+++ src/main/java/it/corvallis/geocoder/utils/App.java
@@ -3,25 +3,25 @@ package it.corvallis.geocoder.utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 public class App {
 
 	private static final Logger logger = LogManager.getLogger(App.class);
-	
+
 	public static void main(String[] args) {
 		String address = args[0];
 		String postalcode = args[1];
 		String city = args[2];
 		String geocoderType = args[3];
-        logger.debug("Parameters : " + address +" "+postalcode+" "+city+" "+geocoderType);
+		logger.debug("Parameters : " + address + " " + postalcode + " " + city + " " + geocoderType);
 
 		try {
 			logger.debug("Provider to use : " + geocoderType);
 			String p = GeocodingService.getGeoCoding(address, postalcode, city, geocoderType);
-			logger.debug("Geometry WKT: "+ p);
+			logger.debug("Geometry WKT: " + p);
 		} catch (Exception e) {
 			logger.debug("Error : " + e.getMessage());
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-}}
+	}
+}
